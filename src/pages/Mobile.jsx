9a09fdddf979cf2271bc500de9e56a269cbeb923
Oguzhan Kayapinar <oguzhankayapinaQar@gmail.com>
2022-11-14 10:11:59 +0300

    mobile page update

--- src/pages/Mobile.jsx
+++ src/pages/Mobile.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
+import Footer from "../components/Footer";
+
+
 
 function Mobile() {
   return (
-    <div>Mobile</div>
+    <div> 
+    <div className="formContainer"></div>
+    <div className='bg-text'> 
+    <h1>MOBİL UYGULAMAMIZ ÇOK YAKINDA</h1>
+    <p className='mobilnav'><Link to="/Login">Ana Sayfaya Geri Dön</Link></p>
+    
+    </div>
+    <Footer/>
+    </div>
   )
 }
 
