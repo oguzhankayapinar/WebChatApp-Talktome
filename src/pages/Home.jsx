d3c72076263ae9cb01e4e0016df59f3afcdb2bff
Oguzhan Kayapinar <oguzhankayapinaQar@gmail.com>
2022-11-16 15:05:09 +0300

    Home update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Sidebar from '../components/Sidebar'
 import Chat from '../components/Chat'
-import LanguageButton from '../components/LanguageButton'
+
 
 const Home = () => {
   return (
