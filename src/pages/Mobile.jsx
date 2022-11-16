5f03b25a50bc7275b1335ea14d4f61e050c82a5e
Oguzhan Kayapinar <oguzhankayapinaQar@gmail.com>
2022-11-16 16:12:31 +0300

    mobile page update

--- src/pages/Mobile.jsx
+++ src/pages/Mobile.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Link } from 'react-router-dom';
 import Footer from "../components/Footer";
 import { useTranslation } from "react-i18next";
-import LanguageButton from "../components/LanguageButton";
+
 
 
 
@@ -13,7 +13,7 @@ function Mobile() {
     <div> 
      
     <div className="formContainer"></div>
-    <LanguageButton/>
+   
     <div className='bg-text'> 
     <h1>{t("mobile")}</h1>
     <p className='mobilnav'><Link to="/Login">{t("pagehome")}</Link></p>
